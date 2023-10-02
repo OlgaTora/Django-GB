2cc8eafe12ade871ee686ee0db8e632cad8dbe8b
olgatorres <bestlaly@mail.ru>
2023-10-02 21:18:09 +0300

    hw4 add min value in form

--- testproject/shop/forms.py
+++ testproject/shop/forms.py
@@ -5,5 +5,5 @@ class AddNewProduct(forms.Form):
     product_name = forms.CharField(max_length=200)
     description = forms.CharField()
     price = forms.FloatField(min_value=0.0)
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField(min_value=0)
     product_image = forms.ImageField()
