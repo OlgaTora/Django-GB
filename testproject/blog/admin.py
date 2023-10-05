5800130086d47ecdf8c16ebcc5aa624f4e976d08
olgatorres <bestlaly@mail.ru>
2023-10-05 12:50:15 +0300

    seminar 5 and hw5 added

--- testproject/blog/admin.py
+++ testproject/blog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Author, Article, Comment
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname', 'birthday']
+    ordering = ['surname']
+    list_filter = ['surname']
+    readonly_fields = ['name']
+
+
+@admin.action(description='Reset views')
+def reset_views(modeladmin, request, queryset):
+    queryset.update(views=0)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish_date', 'views', 'content']
+    ordering = ['author', '-views']
+    list_filter = ['author', 'publish_date']
+    list_per_page = 5
+    actions = [reset_views]
+    readonly_fields = ['author', 'content']
+    search_fields = ['title']
+    search_help_text = 'search'
+    fieldsets = [
+        (
+            'Info about of article',
+            {
+                'classes': ['wide'],
+                'description': 'information',
+                'fields': ['title', 'author'],
+            },
+        ),
+        (
+            'Content of article',
+            {
+                'classes': ['wide'],
+                'description': 'content of article',
+                'fields': ['content'],
+            },
+        ),
+    ]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'article', 'added_date']
+    list_filter = ['author', 'article']
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
--- testproject/shop/admin.py
+++ testproject/shop/admin.py
@@ -1,3 +1,127 @@
+import csv
+import decimal
 from django.contrib import admin
+from django.http import HttpResponse
 
-# Register your models here.
+from .models import Product, Order, Client
+
+
+class OrderInline(admin.TabularInline):
+    model = Order
+
+
+class ClientAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
+    list_display = ['name', 'mail', 'phone', 'registration_date']
+    ordering = ['-registration_date']
+    list_filter = ['name', 'registration_date']
+    readonly_fields = ['name', 'registration_date']
+    search_fields = ['name']
+    search_help_text = 'Search client by name'
+    fieldsets = [
+        (
+            'Main client information',
+            {
+                'classes': ['wide'],
+                'description': 'main information',
+                'fields': ['name', 'registration_date'],
+            },
+        ),
+        (
+            'Contact information',
+            {
+                'classes': ['wide'],
+                'description': 'contacts',
+                'fields': [('phone', 'mail'), 'address'],
+            },
+        ),
+    ]
+
+
+@admin.action(description='Price +10')
+def price_plus_10(modeladmin, request, queryset):
+    for product in queryset:
+        product.price = product.price * decimal.Decimal('1.1')
+        product.save()
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'price', 'quantity']
+    ordering = ['product_name']
+    list_filter = ['product_name']
+    readonly_fields = ['product_name', 'added_date']
+    search_fields = ['description']
+    search_help_text = 'Search product by description'
+    list_per_page = 10
+    actions = [price_plus_10]
+    fieldsets = [
+        (
+            'Main product information',
+            {
+                'classes': ['wide'],
+                'description': 'main information',
+                'fields': ['product_name', 'description', 'added_date'],
+            },
+        ),
+        (
+            'Financial product information',
+            {
+                'classes': ['wide'],
+                'description': 'financial information',
+                'fields': ['price', 'quantity'],
+            },
+        ),
+    ]
+
+
+@admin.action(description='Download orders in csv format')
+def download_orders_2csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="orders.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['Date', 'Summa', 'Status'])
+    orders = queryset.values_list('order_date', 'order_summ', 'status')
+    for order in orders:
+        writer.writerow(order)
+    return response
+
+
+@admin.action(description='Update status for PAID')
+def update_status_paid(modeladmin, request, queryset):
+    queryset.update(status='paid')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_date', 'order_summ', 'status']
+    ordering = ['-order_date']
+    list_filter = ['order_date', 'status']
+    readonly_fields = ['order_date', 'client']
+    search_fields = ['product']
+    search_help_text = 'Search order by product'
+    list_per_page = 10
+    actions = [update_status_paid, download_orders_2csv]
+    fieldsets = [
+        (
+            'Main order information',
+            {
+                'classes': ['wide'],
+                'description': 'main information',
+                'fields': ['order_date', 'order_summ', 'status'],
+            },
+        ),
+        (
+            'Personal order information',
+            {
+                'classes': ['wide'],
+                'description': 'details of order',
+                'fields': ['client', 'product'],
+            },
+        ),
+    ]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Client, ClientAdmin)
